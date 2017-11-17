ef523501ba462c35978703a2b9139243c6f5c585
Conor Raftery <c-raftery@hotmail.com>
2017-11-17 14:45:19 +0000

    Ajax, JS/JQuery 'done', need to work on Eliza's responses

--- ElizaChatBot.go
+++ ElizaChatBot.go
@@ -7,42 +7,33 @@ import (
 	"html/template"
 	"net/http"
 	//"strconv"
-
 )
 
 type myMsg struct {
 	ER string
 	UR string
-	//GuessMessage string
-	//Guess int
 }
-
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Content-Type","text/html")
+	w.Header().Set("Content-Type", "text/html")
 
 	http.ServeFile(w, r, "ChatHomePage.html")
 }
 
-func elizaHandler(w http.ResponseWriter, r *http.Request){
+func elizaHandler(w http.ResponseWriter, r *http.Request) {
 
-
-	elizaResponse :="Hello User, how are you?"
+	elizaResponse := "Hello User, how are you?"
 
 	userResponse := r.FormValue("userResponse")
 
-
-	
-
-
 	t, _ := template.ParseFiles("ElizaChatBot.html")
 
-	t.Execute(w, &myMsg{ER:elizaResponse, UR:userResponse})
+	t.Execute(w, &myMsg{ER: elizaResponse, UR: userResponse})
 }
 
 func main() {
 	http.HandleFunc("/", requestHandler)
-	http.HandleFunc("/guess", elizaHandler)
+	http.HandleFunc("/eliza", elizaHandler)
 	http.ListenAndServe(":8080", nil)
 }
